9187ce2946fed163133d729eae4a9eb81302a00b
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 06:20:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,11 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer= {name: Grace Hopper}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  integer = {id: 2}
 end
 
 def my_hash_creator(key, value)
